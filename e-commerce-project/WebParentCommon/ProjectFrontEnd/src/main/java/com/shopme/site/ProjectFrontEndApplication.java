c8a1729cdab96a71c8e1227ac52a1c94cba69bfa
venky-ezio-42 <116875033+venky-ezio-42@users.noreply.github.com>
2022-12-20 22:02:19 +0530

    Add files via upload

---
+++ e-commerce-project/WebParentCommon/ProjectFrontEnd/src/main/java/com/shopme/site/ProjectFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.shopme.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectFrontEndApplication.class, args);
+	}
+
+}
