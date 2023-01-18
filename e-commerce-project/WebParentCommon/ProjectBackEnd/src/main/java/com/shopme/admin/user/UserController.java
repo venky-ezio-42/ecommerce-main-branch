19d4ad590c1e86d7f15b825c53562a6c84539ee0
venky-ezio-42 <116875033+venky-ezio-42@users.noreply.github.com>
2023-01-18 17:42:20 +0530

    New User page completed
    Styled and linked Create new user to users tab.
    
    Displays successful message when entry happens successfully.

--- e-commerce-project/WebParentCommon/ProjectBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ e-commerce-project/WebParentCommon/ProjectBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -40,11 +41,12 @@ public class UserController {
     }
 
     @PostMapping("users/save")
-    public String saveUser (User createdUser)  {
+    public String saveUser (User createdUser , RedirectAttributes redirectAttributes)  {
         System.out.println(createdUser);
 
         service.save(createdUser);
 
+        redirectAttributes.addFlashAttribute("message" , "User has been added successfully.");
         return "redirect:/users";
     }
 }
