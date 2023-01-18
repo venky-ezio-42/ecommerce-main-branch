a3d69ebe946209e4abc2462d973c159c82b43907
venky-ezio-42 <116875033+venky-ezio-42@users.noreply.github.com>
2023-01-18 17:23:04 +0530

    Create new user page
    Create new user page completed, and data taken as input is stored in MySQL DB.

--- e-commerce-project/WebParentCommon/ProjectBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ e-commerce-project/WebParentCommon/ProjectBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -1,10 +1,12 @@
 package com.shopme.admin.user;
 
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -21,5 +23,28 @@ public class UserController {
         model.addAttribute("listUsers" , listUsers);
 
         return "users";
+    }
+
+    @GetMapping("/users/new")
+    public String newUser(Model model) {
+
+        List<Role> listRoles = service.listRoles();
+
+        User createdUser = new User();
+        createdUser.setEnabled(true);
+
+        model.addAttribute("createdUser" , createdUser);
+        model.addAttribute("listRoles" , listRoles);
+
+        return "user_form";
+    }
+
+    @PostMapping("users/save")
+    public String saveUser (User createdUser)  {
+        System.out.println(createdUser);
+
+        service.save(createdUser);
+
+        return "redirect:/users";
     }
 }
